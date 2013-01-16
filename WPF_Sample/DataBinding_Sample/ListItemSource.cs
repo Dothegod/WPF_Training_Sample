476b0b6112178dbb2a92cd757330ff8979ca8212
Wei Liang <dothegod@gmail.com>
2013-01-16 22:41:26 +0800

    AddItemSourceBinding

---
+++ WPF_Sample/DataBinding_Sample/ListItemSource.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding_Sample
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void ItemSourceBinding()
+        {
+            List<Student> stus = new List<Student>()
+            {
+                new Student(){Id="1", Name="Tom", Age="15"},
+                new Student(){Id="2", Name="Jim", Age="15"},
+                new Student(){Id="3", Name="Lily", Age="15"},
+                new Student(){Id="4", Name="Lucy", Age="15"},
+                new Student(){Id="5", Name="Tim", Age="15"}
+            };
+            listboxStus.ItemsSource = stus;
+            listboxStus.DisplayMemberPath = "Name";
+
+            Binding bd = new Binding("SelectedItem.Id") { Source = listboxStus};
+            tbtItemId.SetBinding(TextBox.TextProperty, bd);
+        }
+    }
+}
--- WPF_Sample/DataBinding_Sample/MainWindow.xaml.cs
+++ WPF_Sample/DataBinding_Sample/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace DataBinding_Sample
         public MainWindow()
         {
             InitializeComponent();
+            ItemSourceBinding();
         }
     }
 }
